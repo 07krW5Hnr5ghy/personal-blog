b81e5c8896587e4ed32121e08f6406edff27ae02
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-10 16:09:51 -0500

    feature(main): authentication middleware

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const PORT = 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(
+    session({
+        secret:'123abc+',
+        resave:false,
+        saveUninitialized:true,
+    })
+);
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'view'));
 
---
+++ middlewares/authentication.js
@@ -0,0 +1,6 @@
+const authMiddleware = (req,res,next) => {
+    if(!req.session.isLoggedIn) return res.redirect('/login');
+    next();
+}
+
+module.exports = {authMiddleware};
