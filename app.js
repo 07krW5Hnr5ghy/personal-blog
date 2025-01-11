4af625cfb60b546d558ce52be17a46937997c041
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-10 19:53:54 -0500

    refactor(main): fix links in templates

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const PORT = 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.use(router);
 app.use(
     session({
         secret:'123abc+',
@@ -16,6 +15,7 @@ app.use(
         saveUninitialized:true,
     })
 );
+app.use(router);
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
 
